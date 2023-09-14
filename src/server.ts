e2eb17fcf593c0deb8f09ab20f4daecd5daeebfa
Douglas Toledo <dwtoledo@outlook.com>
2023-09-14 11:41:20 -0400

    Implement initial Fastify CORS front-end origin

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
 import { getAllPromptsRoute } from './routes/get-all-prompts'
 import { uploadVideosRoute } from './routes/upload-videos'
 import { createTranscriptionRoute } from './routes/create-transcription'
 import { generateAIResultRoute } from './routes/generate-ai-result'
 
 const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideosRoute)
